64e37976eb73656f1f35d400d554c9e2bd00f9de
heesong.shin <heesong.shin@woowahan.com>
2020-11-19 16:58:12 +0900

    - 사용하지 않는 코드 제거 함

--- src/main/java/sion/http/TomcatServerRunner.java
+++ src/main/java/sion/http/TomcatServerRunner.java
@@ -10,17 +10,10 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
-//@Slf4j
 public class TomcatServerRunner {
-//	private PropertiesLoader propertiesLoader;
-//
-//	public TomcatServerRunner() {
-//		propertiesLoader = new PropertiesLoader();
-//	}
 
 	public static void main(String[] args) throws Exception {
 		TomcatServerRunner runner = new TomcatServerRunner();
-		runner.preServerStart();
 		runner.serverStart();
 	}
 
@@ -34,25 +27,17 @@ public class TomcatServerRunner {
 
 		// Declare an alternative location for your "WEB-INF/classes" dir
 		// Servlet 3.0 어노테이션 작동
-		// 참고 :
 		// http://stackoverflow.com/questions/11669507/embedded-tomcat-7-servlet-3-0-annotations-not-working
-		File additionWebInfClasses = new File("bin/main"); // 컴파일된 클래스파일이 있는 위치를 알려주고, 그걸 20번째 줄에 /WEB-INF/classes에 넣어주는
-															// 것.
+		
+		// 컴파일된 클래스파일이 있는 위치를 알려주고, 그걸 20번째 줄에 /WEB-INF/classes에 넣어주는 것.
+		File additionWebInfClasses = new File("bin/main"); 
 		WebResourceRoot resources = new StandardRoot(ctx);
 		// /WEB-INF/classes 밑으로 컴파일된 클래스 파일을 넣어주는 것이 servlet의 스펙
 		resources.addPreResources(
 				new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
 		ctx.setResources(resources);
+		
 		tomcat.start();
 		tomcat.getServer().await();
-	}
-
-	private void preServerStart() {
-		// 서버 시작 전 처리
-		// 1. 설정 정보(포트, DB, controllerFactory 구현체 정보) 가져오기
-//		Properties properties = propertiesLoader.load();
-//		log.debug("PropertiesLoader가 load를 해서.... : {}", properties);
-//
-//		ApplicationContext.addProperties(properties);
 	}
 }
