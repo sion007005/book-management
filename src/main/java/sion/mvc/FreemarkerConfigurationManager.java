77fedca2b57ea9d39fe552b12a50181623321e74
Sion <Sion@DESKTOP-H79A2JN>
2020-12-03 09:40:51 +0900

    사용하지 않는 파일 삭제

--- src/main/java/sion/mvc/FreemarkerConfigurationManager.java
+++ src/main/java/sion/mvc/FreemarkerConfigurationManager.java
@@ -4,7 +4,7 @@ import freemarker.cache.ClassTemplateLoader;
 import freemarker.ext.beans.BeansWrapper;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
-import sion.http.ServerRunner;
+import sion.http.TomcatServerRunner;
 
 public class FreemarkerConfigurationManager { //싱글톤 패턴
 	private static Configuration configuration;
@@ -16,7 +16,7 @@ public class FreemarkerConfigurationManager { //싱글톤 패턴
 	private static void initialize() {
 		if (configuration == null) {
 			configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
-			ClassTemplateLoader loader = new ClassTemplateLoader(ServerRunner.class, "/templates/"); //Runner클래스 기준으로 찾겠다 (위치)
+			ClassTemplateLoader loader = new ClassTemplateLoader(TomcatServerRunner.class, "/templates/"); //TomcatServerRunner클래스 기준으로 찾겠다 (위치)
 			configuration.setTemplateLoader(loader);
 			configuration.setObjectWrapper(new DefaultObjectWrapper(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS));
 			configuration.setObjectWrapper(new BeansWrapper(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS));
